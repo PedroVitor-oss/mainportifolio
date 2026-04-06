58d0825742c4d74bcaa12113d77481659f0df7bf
20223021439-cell <pedro.santos22@escola.pr.gov.br>
2026-04-06 17:02:05 -0300

    mobile header

--- src/components/lyouts/Header.jsx
+++ src/components/lyouts/Header.jsx
@@ -2,20 +2,27 @@ import { useState } from 'react'
 
 import '../../css/Header.css'
 
+function toggleMenu() {
+    const routes = document.querySelector('.routes')
+    routes.classList.toggle('active')
+}
+
 function Header() {
 
     return (
         <>
             <header>
+                <button onClick={toggleMenu}>III</button>
                 <div className="logo">
                     <h1>Pedro Vitor</h1>
+                    
                 </div>
                 <div className="routes">
-                    <a href="#">Inicio</a>
-                    <a href="#About">Sobre</a>
-                    <a href="#Habilidades">Habilidades</a>
-                    <a href="#Works">Trabalhos</a>
-                    <a href="#Contact">Contato</a>
+                    <a onClick={toggleMenu} href="#">Inicio</a>
+                    <a onClick={toggleMenu} href="#About">Sobre</a>
+                    <a onClick={toggleMenu} href="#Habilidades">Habilidades</a>
+                    <a onClick={toggleMenu} href="#Works">Trabalhos</a>
+                    <a onClick={toggleMenu} href="#Contact">Contato</a>
                 </div>
             </header>
         </>
